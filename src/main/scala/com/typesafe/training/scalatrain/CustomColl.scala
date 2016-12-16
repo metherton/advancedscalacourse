168aa00c0b4c1524ce592e3c37b87b7215b1ebdc
metherton <martinetherton63@gmail.com>
2016-12-16 18:54:14 +0100

    add recursion to custom map

--- src/main/scala/com/typesafe/training/scalatrain/CustomColl.scala
+++ src/main/scala/com/typesafe/training/scalatrain/CustomColl.scala
@@ -1,5 +1,7 @@
 package com.typesafe.training.scalatrain
 
+import scala.annotation.tailrec
+
 /**
   * Created by martin on 16/12/16.
   */
@@ -8,8 +10,18 @@ object CustomColl {
 //  def map[A, B](as: List[A])(f: A => B): List[B] =
 //    as.foldLeft(List.empty[B])((bs, a) => bs :+ f(a))
 
-  def map[A, B](as: List[A])(f: A => B): List[B] = as match {
-    case Nil => Nil
-    case x :: xs => f(x) :: map(xs)(f)
+//  def map[A, B](as: List[A])(f: A => B): List[B] = as match {
+//    case Nil => Nil
+//    case x :: xs => f(x) :: map(xs)(f)
+//  }
+
+
+  def map[A, B](as: List[A])(f: A => B): List[B] =  {
+    @tailrec
+    def loop(as: List[A], acc: List[B]): List[B] = as match {
+      case Nil => acc
+      case x :: xs => loop(xs, acc :+ f(x))
+    }
+    loop(as, List.empty[B])
   }
 }
